4a7a782605b0f3a4761b240362749165a87dec02
Бехруз Зоиров <C:\Users\behruz-z\AppData\Roaming\The Bat!>
2024-02-20 10:20:17 +0500

    first commit

--- FR/lesson3.1/index.js
+++ FR/lesson3.1/index.js
@@ -17,7 +17,7 @@ console.log(result);
 const str = "строка  с пробелами ";
 let count = 0;
 for (let i =0; i < str.length; i++){
-    if (str[i] === ''){
+    if (str[i] === ' '){
         count++;
     }
 }
